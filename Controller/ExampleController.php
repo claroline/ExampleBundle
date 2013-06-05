9115ed86e180c1f9f6b0e876d9492584c6dbbe94
ngodfraind <nicolas.godfraind@gmail.com>
2013-06-05 13:25:34 +0200

    Updating ExampleBundle

--- Controller/ExampleController.php
+++ Controller/ExampleController.php
@@ -22,15 +22,11 @@ class ExampleController extends Controller
         $em = $this->get('doctrine.orm.entity_manager');
         //get the resource
         $resource = $em->getRepository('ClarolineCoreBundle:Resource\AbstractResource')->find($exampleId);
-        //get the current workspace.
-        //if you only have the workspace id =>
-        //$em->getRepository('Claroline\CoreBundle\Workspace\AbstractWorkspace')->find(...);
-        $workspace = $resource->getWorkspace();
 
         //get the text.
         return $this->render(
             'ClarolineExampleBundle::resource.html.twig',
-            array('resource' => $resource, 'workspace' => $workspace, '_resource' => $resource)
+            array('_resource' => $resource)
         );
     }
 }
